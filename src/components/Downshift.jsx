73e36de9680a0ebdd76f1ffbece62f1bc9f4ab8e
Olli Nieminen <EXT-Olli.Nieminen@hsl.fi>
2020-08-05 11:59:27 +0300

    Create screen reader test environment for Downshift using Create React App

---
+++ src/components/Downshift.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Downshift from "downshift";
+
+export const DownshiftExample = () => {
+  const items = [
+    { value: "value1", id: "id1" },
+    { value: "value2", id: "id2" },
+    { value: "value3", id: "id3" },
+  ];
+
+  const itemToString = (item) => (item.value ? item.value : "");
+  return (
+    <div>
+      <h1>Downshift Example</h1>
+      <div>
+        <Downshift itemToString={itemToString}>
+          {({
+            getLabelProps,
+            getInputProps,
+            getMenuProps,
+            getItemProps,
+            getToggleButtonProps,
+            isOpen,
+            highlightedIndex,
+          }) => (
+            <div>
+              <label {...getLabelProps()}> Test Screen Reader</label>
+
+              <input
+                readOnly={true}
+                placeholder="select"
+                {...getInputProps()}
+              ></input>
+              <button {...getToggleButtonProps()}>
+                {isOpen ? "Close Menu" : "Open Menu"}
+              </button>
+
+              <ul {...getMenuProps()}>
+                {isOpen
+                  ? items.map((item, i) => (
+                      <li
+                        key={i}
+                        {...getItemProps({
+                          item,
+                          key: item.id,
+                          style: {
+                            backgroundColor:
+                              highlightedIndex === i ? "green" : "blue",
+                          },
+                        })}
+                      >
+                        {item.value}
+                      </li>
+                    ))
+                  : null}
+              </ul>
+            </div>
+          )}
+        </Downshift>
+      </div>
+    </div>
+  );
+};
